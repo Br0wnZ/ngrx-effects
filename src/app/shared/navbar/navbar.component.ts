7cda171286435e1dce4d1537c2b4bafa9644d21e
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2021-01-13 11:21:06 +0100

    App routes implemented

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild('txtInput') txtInput: ElementRef;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  searchUser = (id: string) =>
+    id
+      ? (this.router.navigate(['/user', id]),
+        (this.txtInput.nativeElement.value = null))
+      : '';
 }
